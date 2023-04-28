a9afed4209e67b19c67c430855bcf026d4c56fd4
Connor <connor.cs@protonmail.com>
2023-04-28 18:16:04 -0400

    fixed bug

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,11 +4,14 @@ import { IoMoonOutline, IoMoon } from "react-icons/io5";
 import { useNavigate } from 'react-router-dom';
 
 export default function Header({ darkMode, setDarkMode }) {
-  const navigate=useNavigate()
+  const navigate = useNavigate()
   return (
-    <div className={darkMode ? 'darkMode-header header' : 'lightMode-header header'} onClick={()=>navigate('/')}>
+    <div className={darkMode ? 'darkMode-header header' : 'lightMode-header header'} onClick={() => navigate('/')}>
       <h1>Where in the world?</h1>
-      <button className={darkMode ? 'darkMode-button button' : 'lightMode-button button'} onClick={() => setDarkMode(!darkMode)}>
+      <button className={darkMode ? 'darkMode-button button' : 'lightMode-button button'} onClick={(e) => {
+        e.stopPropagation()
+        setDarkMode(!darkMode)
+      }}>
         {darkMode ? <IoMoon className={darkMode ? 'darkMode icon' : 'lightMode icon'} size={18} /> : <IoMoonOutline className={darkMode ? 'darkMode icon' : 'lightMode icon'} size={18} />}
         Dark Mode
       </button>
